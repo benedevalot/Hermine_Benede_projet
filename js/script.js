0e1ed660d21cb8bb5355c34f31c2001094568cb3
benedevalot <2713117@collegelacite.ca>
2023-08-06 07:52:18 -0400

    Premier comit

---
+++ js/script.js
@@ -0,0 +1,1059 @@
+let tab1 =[];
+for (let i = 0; i < 15; i++) 
+{tab1.push([]);
+    for (let j = 0; j < 25; j++) {
+        tab1[i].push('m');
+    }
+}
+tab1[5][16]="M";
+tab1[11][19]="M";
+
+tab1[1][14]="t";
+tab1[9][10]="t";
+
+tab1[13][20]="j";
+
+tab1[1][15]="o";
+tab1[1][16]="o";
+tab1[1][17]="o";
+
+tab1[2][14]="o";
+tab1[2][15]="o";
+tab1[2][16]="o";
+tab1[2][17]="o";
+
+tab1[3][17]="o";
+
+tab1[4][17]="o";
+tab1[4][18]="o";
+
+tab1[5][14]="o";
+tab1[5][15]="o";
+tab1[5][17]="o";
+tab1[5][18]="o";
+tab1[5][19]="o";
+
+
+tab1[6][14]="o";
+tab1[6][15]="o";
+tab1[6][16]="o";
+tab1[6][17]="o";
+tab1[6][18]="o";
+tab1[6][19]="o";
+
+tab1[7][14]="o";
+tab1[7][18]="o";
+
+tab1[8][11]="o";
+tab1[8][12]="o";
+tab1[8][14]="o";
+tab1[8][18]="o";
+
+tab1[9][11]="o";
+tab1[9][12]="o";
+tab1[9][13]="o";
+tab1[9][14]="o";
+tab1[9][18]="o";
+tab1[9][19]="o";
+
+tab1[10][10]="o";
+tab1[10][11]="o";
+tab1[10][12]="o";
+tab1[10][18]="o";
+tab1[10][19]="o";
+tab1[10][20]="o";
+
+tab1[11][20]="o";
+
+tab1[12][19]="o";
+tab1[12][20]="o";
+
+tab1[13][19]="o";
+tab1[13][21]="o";
+
+
+let tab2 =[];
+for (let i = 0; i < 15; i++) 
+{tab2.push([]);
+    for (let j = 0; j < 25; j++) {
+        tab2[i].push('m');
+    }
+}
+tab2[5][16]="t";
+tab2[11][19]="t";
+
+tab2[1][14]="M";
+tab2[9][10]="M";
+
+tab2[13][21]="o";
+
+tab2[1][15]="o";
+tab2[1][16]="o";
+tab2[1][17]="o";
+
+tab2[2][14]="o";
+tab2[2][15]="o";
+tab2[2][16]="o";
+tab2[2][17]="o";
+
+tab2[3][17]="o";
+
+tab2[4][17]="o";
+tab2[4][18]="o";
+
+tab2[5][14]="o";
+tab2[5][15]="j";
+tab2[5][17]="o";
+tab2[5][18]="o";
+tab2[5][19]="o";
+
+
+tab2[6][14]="o";
+tab2[6][15]="o";
+tab2[6][16]="o";
+tab2[6][17]="o";
+tab2[6][18]="o";
+tab2[6][19]="o";
+
+tab2[7][14]="o";
+tab2[7][18]="o";
+
+tab2[8][11]="o";
+tab2[8][12]="o";
+tab2[8][14]="o";
+tab2[8][18]="o";
+
+tab2[9][11]="o";
+tab2[9][12]="o";
+tab2[9][13]="o";
+tab2[9][14]="o";
+tab2[9][18]="o";
+tab2[9][19]="o";
+
+tab2[10][10]="o";
+tab2[10][11]="o";
+tab2[10][12]="o";
+tab2[10][18]="o";
+tab2[10][19]="o";
+tab2[10][20]="o";
+
+tab2[11][20]="o";
+
+tab2[12][19]="o";
+tab2[12][20]="o";
+
+tab2[13][20]="o";
+tab2[13][19]="o";
+
+
+
+let tab3 =[];
+for (let i = 0; i < 15; i++) 
+{tab3.push([]);
+    for (let j = 0; j < 25; j++) {
+        tab3[i].push('m');
+    }
+}
+tab3[5][11]="t";
+tab3[11][14]="M";
+
+tab3[1][9]="t";
+tab3[9][5]="M";
+
+tab3[13][15]="o";
+
+tab3[1][10]="o";
+tab3[1][11]="o";
+tab3[1][12]="o";
+
+tab3[2][9]="o";
+tab3[2][10]="o";
+tab3[2][11]="o";
+tab3[2][12]="o";
+
+tab3[3][12]="o";
+
+tab3[4][12]="o";
+tab3[4][13]="o";
+
+tab3[5][9]="o";
+tab3[5][10]="o";
+tab3[5][12]="o";
+tab3[5][13]="j";
+tab3[5][14]="o";
+
+
+tab3[6][9]="o";
+tab3[6][10]="o";
+tab3[6][11]="o";
+tab3[6][12]="o";
+tab3[6][13]="o";
+tab3[6][14]="o";
+
+tab3[7][9]="o";
+tab3[7][13]="o";
+
+tab3[8][6]="o";
+tab3[8][7]="o";
+tab3[8][9]="o";
+tab3[8][13]="o";
+
+tab3[9][6]="o";
+tab3[9][7]="o";
+tab3[9][8]="o";
+tab3[9][9]="o";
+tab3[9][13]="o";
+tab3[9][14]="o";
+
+tab3[10][5]="o";
+tab3[10][6]="o";
+tab3[10][7]="o";
+tab3[10][13]="o";
+tab3[10][14]="o";
+tab3[10][15]="o";
+
+tab3[11][15]="o";
+
+tab3[12][14]="o";
+tab3[12][15]="o";
+
+tab3[13][14]="o";
+tab3[13][16]="o";
+
+
+
+let tab4 =[];
+for (let i = 0; i < 15; i++) 
+{tab4.push([]);
+    for (let j = 0; j < 25; j++) {
+        tab4[i].push('m');
+    }
+}
+tab4[5][13]="o";
+tab4[11][16]="o";
+
+tab4[1][11]="o";
+tab4[9][7]="o";
+
+tab4[13][17]="o";
+
+tab4[1][12]="o";
+tab4[1][13]="o";
+tab4[1][14]="o";
+
+tab4[2][11]="o";
+tab4[2][12]="o";
+tab4[2][13]="M";
+tab4[2][14]="o";
+
+tab4[3][14]="o";
+
+tab4[4][14]="o";
+tab4[4][15]="o";
+
+tab4[5][11]="o";
+tab4[5][12]="o";
+tab4[5][14]="o";
+tab4[5][15]="o";
+tab4[5][16]="o";
+
+
+tab4[6][11]="o";
+tab4[6][12]="o";
+tab4[6][13]="j";
+tab4[6][14]="o";
+tab4[6][15]="o";
+tab4[6][16]="o";
+
+tab4[7][11]="o";
+tab4[7][15]="o";
+
+tab4[8][8]="o";
+tab4[8][9]="o";
+tab4[8][11]="o";
+tab4[8][15]="o";
+
+tab4[9][8]="o";
+tab4[9][9]="o";
+tab4[9][10]="o";
+tab4[9][11]="t";
+tab4[9][15]="o";
+tab4[9][16]="o";
+
+tab4[10][7]="o";
+tab4[10][8]="o";
+tab4[10][9]="o";
+tab4[10][15]="o";
+tab4[10][16]="o";
+tab4[10][17]="o";
+
+tab4[11][17]="o";
+
+tab4[12][16]="t";
+tab4[12][17]="o";
+
+tab4[13][16]="o";
+tab4[13][15]="M";
+
+
+
+let tab5 =[];
+for (let i = 0; i < 15; i++) 
+{tab5.push([]);
+    for (let j = 0; j < 25; j++) {
+        tab5[i].push('m');
+    }
+}
+tab5[5][15]="o";
+tab5[11][18]="M";
+
+tab5[1][13]="o";
+tab5[9][9]="t";
+
+tab5[13][17]="o";
+
+tab5[1][14]="o";
+tab5[1][15]="o";
+tab5[1][16]="o";
+
+tab5[2][13]="j";
+tab5[2][14]="o";
+tab5[2][15]="o";
+tab5[2][16]="o";
+
+tab5[3][16]="o";
+
+tab5[4][16]="o";
+tab5[4][17]="o";
+
+tab5[5][13]="o";
+tab5[5][14]="o";
+tab5[5][16]="o";
+tab5[5][17]="o";
+tab5[5][18]="o";
+
+
+tab5[6][13]="o";
+tab5[6][14]="o";
+tab5[6][15]="o";
+tab5[6][16]="o";
+tab5[6][17]="o";
+tab5[6][18]="t";
+
+tab5[7][13]="o";
+tab5[7][17]="o";
+
+tab5[8][10]="o";
+tab5[8][11]="o";
+tab5[8][13]="o";
+tab5[8][17]="o";
+
+tab5[9][10]="o";
+tab5[9][11]="o";
+tab5[9][12]="o";
+tab5[9][13]="o";
+tab5[9][17]="M";
+tab5[9][18]="o";
+
+tab5[10][9]="o";
+tab5[10][10]="o";
+tab5[10][11]="o";
+tab5[10][17]="o";
+tab5[10][18]="o";
+tab5[10][19]="o";
+
+tab5[11][19]="o";
+
+tab5[12][18]="o";
+tab5[12][19]="o";
+
+tab5[13][16]="o";
+tab5[13][18]="o";
+
+
+
+let tab6 =[];
+for (let i = 0; i < 15; i++) 
+{tab6.push([]);
+    for (let j = 0; j < 25; j++) {
+        tab6[i].push('m');
+    }
+}
+tab6[5][16]="t";
+tab6[11][19]="M";
+
+tab6[1][14]="t";
+tab6[9][10]="M";
+
+tab6[1][15]="o";
+tab6[1][16]="o";
+tab6[1][17]="o";
+
+tab6[2][14]="o";
+tab6[2][15]="o";
+tab6[2][16]="o";
+tab6[2][17]="o";
+
+tab6[3][17]="o";
+
+tab6[4][17]="o";
+tab6[4][18]="o";
+
+tab6[5][14]="o";
+tab6[5][15]="o";
+tab6[5][17]="o";
+tab6[5][18]="o";
+tab6[5][19]="o";
+
+
+tab6[6][14]="o";
+tab6[6][15]="o";
+tab6[6][16]="o";
+tab6[6][17]="o";
+tab6[6][18]="o";
+tab6[6][19]="o";
+
+tab6[7][14]="o";
+tab6[7][18]="o";
+
+tab6[8][11]="o";
+tab6[8][12]="j";
+tab6[8][14]="o";
+tab6[8][18]="o";
+
+tab6[9][11]="o";
+tab6[9][12]="o";
+tab6[9][13]="o";
+tab6[9][14]="o";
+tab6[9][18]="o";
+tab6[9][19]="o";
+
+tab6[10][10]="o";
+tab6[10][11]="o";
+tab6[10][12]="o";
+tab6[10][18]="o";
+tab6[10][19]="o";
+tab6[10][20]="o";
+
+tab6[11][20]="o";
+
+tab6[12][19]="o";
+tab6[12][20]="o";
+
+tab6[1][13]="o";
+tab6[1][12]="o";
+
+let tab7 =[];
+for (let i = 0; i < 15; i++) 
+{tab7.push([]);
+    for (let j = 0; j < 25; j++) {
+        tab7[i].push('m');
+    }
+}
+tab7[5][7]="M";
+tab7[11][10]="t";
+
+tab7[1][5]="t";
+tab7[9][1]="M";
+
+tab7[13][11]="o";
+
+tab7[1][6]="o";
+tab7[1][7]="o";
+tab7[1][8]="o";
+
+tab7[2][5]="o";
+tab7[2][6]="o";
+tab7[2][7]="o";
+tab7[2][8]="o";
+
+tab7[3][8]="o";
+
+tab7[4][8]="o";
+tab7[4][9]="o";
+
+tab7[5][5]="o";
+tab7[5][6]="o";
+tab7[5][8]="o";
+tab7[5][9]="o";
+tab7[5][10]="o";
+
+
+tab7[6][5]="o";
+tab7[6][6]="o";
+tab7[6][7]="o";
+tab7[6][8]="o";
+tab7[6][9]="o";
+tab7[6][10]="o";
+
+tab7[7][5]="o";
+tab7[7][9]="o";
+
+tab7[8][2]="o";
+tab7[8][3]="o";
+tab7[8][5]="j";
+tab7[8][9]="o";
+
+tab7[9][2]="o";
+tab7[9][3]="o";
+tab7[9][4]="o";
+tab7[9][5]="o";
+tab7[9][9]="o";
+tab7[9][10]="o";
+
+tab7[10][1]="o";
+tab7[10][2]="o";
+tab7[10][3]="o";
+tab7[10][9]="o";
+tab7[10][10]="o";
+tab7[10][11]="o";
+
+tab7[11][11]="o";
+
+tab7[12][10]="o";
+tab7[12][11]="o";
+
+tab7[13][10]="o";
+tab7[13][12]="o";
+
+
+
+
+
+let tab8 =[];
+for (let i = 0; i < 15; i++) 
+{tab8.push([]);
+    for (let j = 0; j < 25; j++) {
+        tab8[i].push('m');
+    }
+}
+tab8[5][7]="M";
+tab8[11][10]="M";
+
+tab8[1][5]="t";
+tab8[9][1]="t";
+
+tab8[13][11]="o";
+
+tab8[1][6]="o";
+tab8[1][7]="o";
+tab8[1][8]="o";
+
+tab8[2][5]="o";
+tab8[2][6]="o";
+tab8[2][7]="o";
+tab8[2][8]="o";
+
+tab8[3][8]="o";
+
+tab8[4][8]="o";
+tab8[4][9]="o";
+
+tab8[5][5]="o";
+tab8[5][6]="o";
+tab8[5][8]="o";
+tab8[5][9]="o";
+tab8[5][10]="o";
+
+
+tab8[6][5]="o";
+tab8[6][6]="o";
+tab8[6][7]="o";
+tab8[6][8]="j";
+tab8[6][9]="o";
+tab8[6][10]="o";
+
+tab8[7][5]="o";
+tab8[7][9]="o";
+
+tab8[8][2]="o";
+tab8[8][3]="o";
+tab8[8][5]="o";
+tab8[8][9]="o";
+
+tab8[9][2]="o";
+tab8[9][3]="o";
+tab8[9][4]="o";
+tab8[9][5]="o";
+tab8[9][9]="o";
+tab8[9][10]="o";
+
+tab8[10][1]="o";
+tab8[10][2]="o";
+tab8[10][3]="o";
+tab8[10][9]="o";
+tab8[10][10]="o";
+tab8[10][11]="o";
+
+tab8[11][11]="o";
+
+tab8[12][10]="o";
+tab8[12][11]="o";
+
+tab8[13][10]="o";
+tab8[13][12]="o";
+
+
+
+let tab9 =[];
+for (let i = 0; i < 15; i++) 
+{tab9.push([]);
+    for (let j = 0; j < 25; j++) {
+        tab9[i].push('m');
+    }
+}
+tab9[5][18]="o";
+tab9[11][21]="o";
+
+tab9[1][16]="o";
+tab9[9][12]="o";
+
+tab9[13][22]="M";
+
+tab9[1][17]="o";
+tab9[1][18]="o";
+tab9[1][19]="o";
+
+tab9[2][16]="o";
+tab9[2][17]="o";
+tab9[2][18]="o";
+tab9[2][19]="o";
+
+tab9[3][19]="t";
+
+tab9[4][19]="M";
+tab9[4][20]="o";
+
+tab9[5][16]="o";
+tab9[5][17]="o";
+tab9[5][19]="o";
+tab9[5][20]="o";
+tab9[5][21]="o";
+
+
+tab9[6][16]="o";
+tab9[6][17]="o";
+tab9[6][18]="o";
+tab9[6][19]="o";
+tab9[6][20]="o";
+tab9[6][21]="o";
+
+tab9[7][16]="o";
+tab9[7][20]="o";
+
+tab9[8][13]="o";
+tab9[8][14]="o";
+tab9[8][16]="o";
+tab9[8][20]="o";
+
+tab9[9][13]="o";
+tab9[9][14]="o";
+tab9[9][15]="j";
+tab9[9][16]="o";
+tab9[9][20]="o";
+tab9[9][21]="o";
+
+tab9[10][12]="o";
+tab9[10][13]="o";
+tab9[10][15]="o";
+tab9[10][20]="o";
+tab9[10][21]="o";
+tab9[10][22]="o";
+
+tab9[11][22]="o";
+
+tab9[12][21]="o";
+tab9[12][22]="o";
+
+tab9[13][21]="o";
+tab9[13][23]="t";
+
+let tab10 =[];
+for (let i = 0; i < 15; i++) 
+{tab10.push([]);
+    for (let j = 0; j < 25; j++) {
+        tab10[i].push('m');
+    }
+}
+tab10[5][16]="o";
+tab10[11][19]="t";
+
+tab10[1][14]="M";
+tab10[9][10]="o";
+
+tab10[13][20]="o";
+
+tab10[1][15]="o";
+tab10[1][16]="o";
+tab10[1][17]="o";
+
+tab10[2][14]="o";
+tab10[2][15]="o";
+tab10[2][16]="o";
+tab10[2][17]="o";
+
+tab10[3][17]="o";
+
+tab10[4][17]="M";
+tab10[4][18]="o";
+
+tab10[5][14]="o";
+tab10[5][15]="o";
+tab10[5][17]="o";
+tab10[5][18]="j";
+tab10[5][19]="o";
+
+tab10[5][20]="o";
+tab10[5][21]="o";
+tab10[5][22]="o";
+tab10[5][23]="t";
+
+
+
+tab10[6][14]="o";
+tab10[6][15]="o";
+tab10[6][16]="o";
+tab10[6][17]="o";
+tab10[6][18]="o";
+tab10[6][19]="o";
+
+tab10[6][20]="o";
+tab10[6][21]="o";
+tab10[6][22]="o";
+tab10[6][23]="o";
+
+tab10[7][14]="o";
+tab10[7][18]="o";
+
+tab10[8][11]="o";
+tab10[8][12]="o";
+tab10[8][14]="o";
+tab10[8][18]="o";
+
+tab10[9][11]="o";
+tab10[9][12]="o";
+tab10[9][13]="o";
+tab10[9][14]="o";
+tab10[9][18]="o";
+tab10[9][19]="o";
+
+tab10[10][10]="o";
+tab10[10][11]="o";
+tab10[10][12]="o";
+tab10[10][18]="o";
+tab10[10][19]="o";
+tab10[10][20]="o";
+
+tab10[11][20]="o";
+
+tab10[12][19]="o";
+tab10[12][20]="o";
+
+tab10[13][19]="o";
+
+
+function tableauAleatoire() {
+  const randomNumber = Math.floor(Math.random() * 10) + 1;
+  let tableau;
+
+  switch (randomNumber) {
+    case 1:
+      tableau = tab1;
+      break;
+    case 2:
+      tableau = tab2;
+      break;
+    case 3:
+      tableau = tab3;
+      break;
+    case 4:
+      tableau = tab4;
+      break;
+    case 5:
+      tableau = tab5;
+      break;
+    case 6:
+      tableau = tab6;
+      break;
+    case 7:
+      tableau = tab7;
+      break;
+    case 8:
+      tableau = tab8;
+      break;
+    case 9:
+      tableau = tab9;
+      break;
+    case 10:
+      tableau = tab10;
+      break;
+    default:
+      console.error("Erreur : numéro de tableau invalide");
+  }
+
+  return tableau;
+}
+
+let score = 0;
+let trésorsRestants = 2;
+// console.table(tableau);
+
+const tableau = tableauAleatoire();
+
+function updateTable(){ 
+
+
+  
+  document.getElementById("tbody").innerHTML = "";
+
+
+
+
+
+for (let i = 0; i < tableau.length; i++) {
+    let elementTr = document.createElement("tr");
+
+    for (let j = 0; j < tableau[i].length; j++) {
+        let elementTd = document.createElement("td");
+        elementTd.innerText = tableau[i][j]; 
+       console.log (tableau[i][j]);
+
+       elementTr.appendChild(elementTd);
+ elementTd.style.borderCollapse = "separate";
+ elementTd.style.borderSpacing = "5px"; 
+ elementTd.style.border = "1px solid white";
+ elementTd.style.width='25px';
+ elementTd.style.height='25px';
+ elementTd.style.textAlign='center';
+ elementTd.style.color='transparent';
+ 
+
+
+ if(tableau[i][j] == "m"){
+  elementTd.classList.add("imagemur");
+} else if (tableau[i][j] == "o"){
+  elementTd.style.backgroundColor = "white";
+  }
+  else if (tableau[i][j] == "M"){
+      elementTd.classList.add("imagemonstre") ;
+
+}
+else if (tableau[i][j] == "t") {
+  elementTd.classList.add("imagetresor");
+}
+
+else{
+  elementTd.classList.add("imagejoueur");
+
+}
+
+}
+       document.getElementById("tbody").appendChild(elementTr);
+
+    }
+    
+  // Afficharge  du nombre de trésors restants
+  const trésorsRestantsSpan = document.getElementById("tresorsRestants");
+  trésorsRestantsSpan.textContent = trésorsRestants;
+
+   // Afficharge  du score
+   const Monscore = document.getElementById("lescore");
+   Monscore.textContent =score;
+ 
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  updateTable();
+
+
+
+
+
+    function deplacer(direction) {
+        let joueurPosition = trouverPosition("j");
+
+        let nextPosition;
+      
+        switch (direction) {
+          case "haut":
+            nextPosition = { x: joueurPosition.x, y: joueurPosition.y - 1 };
+            break;
+          case "bas":
+            nextPosition = { x: joueurPosition.x, y: joueurPosition.y + 1 };
+            break;
+          case "gauche":
+            nextPosition = { x: joueurPosition.x - 1, y: joueurPosition.y };
+            break;
+          case "droite":
+            nextPosition = { x: joueurPosition.x + 1, y: joueurPosition.y };
+            break;
+          default:
+            return;
+        }
+
+  
+
+        if (!estDansLaGrille(nextPosition) || tableau[nextPosition.y][nextPosition.x] === "m") {
+          return; // Ne pas autoriser le déplacement vers un mur ou à l'extérieur de la grille
+        }
+      
+        // Vérifier si le joueur a trouvé un trésor
+        if (tableau[nextPosition.y][nextPosition.x] === "t") {
+          score++;
+          trésorsRestants--;
+        
+        }
+      
+        // Vérifier si le joueur est sur une cellule avec un monstre
+        if (tableau[nextPosition.y][nextPosition.x] === "M") {
+          gameOver();
+          return;
+        }
+      
+        // Mettre à jour la position du joueur sur la grille
+        tableau[joueurPosition.y][joueurPosition.x] = "o";
+        tableau[nextPosition.y][nextPosition.x] = "j";
+      
+        // Mettre à jour l'affichage du tableau
+        updateTable();
+      
+        // Vérifier si le joueur a gagné la partie
+        if (trésorsRestants === 0) {
+          gameWin();
+        }
+      }
+      
+      function deplacerMonstres() {
+
+
+        for (let i = 0; i < tableau.length; i++) {
+            for (let j = 0; j < tableau[i].length; j++) {
+              if (tableau[i][j] === "M") {
+                let randomDirection = Math.random() < 0.5 ? "gauche_droite" : "haut_bas";
+        
+                let nextPosition;
+        
+                switch (randomDirection) {
+                  case "gauche_droite":
+                    nextPosition = { x: j + (Math.random() < 0.5 ? -1 : 1), y: i };
+                    break;
+                  case "haut_bas":
+                    nextPosition = { x: j, y: i + (Math.random() < 0.5 ? -1 : 1) };
+                    break;
+                  default:
+                    break;
+                }
+        
+                if (estDansLaGrille(nextPosition) && tableau[nextPosition.y][nextPosition.x] !== "m") {
+                   
+                    if (tableau[nextPosition.y][nextPosition.x] === "t") {
+                        return;
+                      }
+
+                   
+                        if (tableau[nextPosition.y][nextPosition.x] === "M") {
+                            return;
+                          }
+
+                 else if (tableau[nextPosition.y][nextPosition.x] === "j") {
+                    gameOver();
+                    return;
+                  }
+                  tableau[i][j] = "o";
+                  tableau[nextPosition.y][nextPosition.x] = "M";
+                }
+              }
+            }
+          }
+        
+          updateTable();
+      }
+      
+      function trouverPosition(element) {
+        // Le code pour trouver la position du joueur sur la grille (
+            for (let i = 0; i < tableau.length; i++) {
+                for (let j = 0; j < tableau[i].length; j++) {
+                  if (tableau[i][j] === element) {
+                    return { x: j, y: i };
+                  }
+                }
+              }
+              return null;
+      }
+      
+      function estDansLaGrille(position) {
+        // Le code pour vérifier si une position est à l'intérieur de la grille
+        
+  return position.x >= 0 && position.x < tableau[0].length && position.y >= 0 && position.y < tableau.length;
+
+      }
+      
+      function gameOver() {
+        // Le code pour afficher un message de fin de partie en cas de défaite du joueur 
+        alert("Fin du jeu!partie terninee, Vous avez été capturé par un monstre.");
+        document.getElementById("boutonHaut").disabled = true;
+        document.getElementById("boutonBas").disabled = true;
+        document.getElementById("boutonGauche").disabled = true;
+        document.getElementById("boutonDroite").disabled = true;
+      }
+      
+      function gameWin() {
+        // Le code pour afficher un message de victoire en cas de réussite du joueur
+
+        alert("CONGRATULATION!  Vous avez decouvert tous les trésors et gagné la partie. votre score est de :"+score +" vous pouvez commencez une nouvelle partie en appuyant sur le bouton Reinitialiser");
+        document.getElementById("boutonHaut").disabled = true;
+        document.getElementById("boutonBas").disabled = true;
+        document.getElementById("boutonGauche").disabled = true;
+        document.getElementById("boutonDroite").disabled = true;
+      }
+      
+   
+        
+      
+        // Ajouter les événements onclick aux boutons pour les déplacements du joueur
+        const boutonHaut = document.getElementById("boutonHaut");
+        boutonHaut.onclick = function () {
+          deplacer("haut");
+          deplacerMonstres();
+        };
+      
+        const boutonBas = document.getElementById("boutonBas");
+        boutonBas.onclick = function () {
+          deplacer("bas");
+          deplacerMonstres();
+        };
+      
+        const boutonGauche = document.getElementById("boutonGauche");
+        boutonGauche.onclick = function () {
+          deplacer("gauche");
+          deplacerMonstres();
+        };
+      
+        const boutonDroite = document.getElementById("boutonDroite");
+        boutonDroite.onclick = function () {
+          deplacer("droite");
+          deplacerMonstres();
+        };
+      
+        // Ajouter l'événement onclick pour le bouton de réinitialisation
+        const boutonReinitialiser = document.getElementById("boutonReinitialiser");
+        boutonReinitialiser.onclick = function () {
+          reinitialiser();
+        };
+      });
+
+     
+
+    
+      function reinitialiser() {
+        location.reload();
+    
+      }
+        
+
+
+    
+        
+        
+
+       
